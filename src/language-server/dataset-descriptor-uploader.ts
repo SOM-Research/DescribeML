623898c78640e8a9d3ff1f83b1b50aed6fb167aa
Joan <giner.joan@gmail.com>
2022-02-18 13:01:38 +0100

    Added completness and unique values calculus when a data is uploaded

--- src/language-server/dataset-descriptor-uploader.ts
+++ src/language-server/dataset-descriptor-uploader.ts
@@ -3,6 +3,7 @@ import { parse } from 'csv-parse/sync';
 import fs from 'fs';
 
 
+
 export interface Uploader {
     uploadDataset(filepath: string) :   Promise<string> ;
     buildSnippet(data: Array<any>, filepath: string): string; 
@@ -32,11 +33,13 @@ export class DatasetUploader implements Uploader {
 
     buildSnippet(data: Array<any>, filepath: string) {
         // Get Headers
-        let headers: Array<string> = data[0];
+        const headers: Array<string> = data[0];
         // Get number of rows
-        let numberofResults = data.length - 1
+        const numberofResults = data.length - 1
         // Can we infer the most probable type?
-        // Can we calculate attribute completeness?
+   
+    
+
         // If number, can we calculate ranges?
         // Can we count the number of empty or NANs values in a attibute column?
         let body:string= `
@@ -46,11 +49,43 @@ export class DatasetUploader implements Uploader {
             \ttotal number ${numberofResults}
             \twithAttributes:\n`;
 
-        headers.forEach(attr => {
-           body = body + `\t\t\t\t\tattribute  ${attr.replaceAll(' ','_')} 
+        headers.forEach((attr, index) => {
+            // Can we calculate attribute completeness?
+            let attrData = data.map(function(value,index2) { return value[index]; });
+            // Completness
+            const completness = this.attributeCompletness(attrData);
+            // Unique
+            const unique = this.attributeUnique(attrData);
+            // If number Mean and Standard Desviation
+            // If String word count
+            console.log(completness);
+            body = body + `\t\t\t\t\tattribute  ${attr.replaceAll(' ','_')} 
                         ofType string  
-                        description: \"Describe the attribute \"\n`;
+                        description: \"Describe the attribute with ${unique} unique elements and a completness of ${completness} \"\n`;
         });
         return body;
+    }
+    attributeUnique(attrData: Array<any>) {
+         // delete headers
+         attrData.shift();
+         let uniques = attrData.filter((v,i,a) => a.indexOf(v) === i);
+         return uniques.length;
+    }
+
+    attributeCompletness(attrData: Array<any>){
+        // delete headers
+        attrData.shift();
+        let validValues = 0;
+        attrData.forEach(element => {
+            if (element){
+                console.log(element)
+            }
+            if (element === undefined || element === null || element == '' || element == 'NaN') {
+            } else {
+                validValues = validValues+1;
+            }
+        });
+        if (validValues != 0) return (validValues/attrData.length).toFixed(2);
+        else return 0
     }
 }
